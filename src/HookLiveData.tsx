f1e95c5e95bf2e8a5b100839ec57202de1839160
BrendanOswego <brendan.goldberg@cainkade.com>
2020-07-19 16:30:28 -0400

    chore: update doc

--- src/HookLiveData.tsx
+++ src/HookLiveData.tsx
@@ -1,32 +1,28 @@
 import * as React from "react"
 
-import {LiveData, Observer} from "@ts-livedata/core"
+import {MutableLiveData, LiveData, Observer} from "@ts-livedata/core"
 import {useEffect} from "react";
 
+/**
+ * React Hook LiveData (similar to {@link MutableLiveData}) which removes observers automatically once unmounted.
+ */
 class HookLiveData<T> extends LiveData<T> {
-
-    readonly DEBUG: Boolean = true
 
     /**
      * Attaches and detaches Observer to React functional component.
      *
      * @remarks
      * Should be used within scope of React Hooks, not {@link React.Component}.
+     * Otherwise runtime error will be thrown from React.
      *
      * @param observer  The observer to watch for changes.
      */
     observe(observer: Observer<T>) {
         useEffect(() => {
-            if (this.DEBUG) {
-                console.log("adding observer")
-            }
             this.observers.push(observer)
             const tData = this.data
             if (tData != null) observer(tData)
             return () => {
-                if (this.DEBUG) {
-                    console.log("removing observer")
-                }
                 this.removeObserver(observer)
             }
         }, [])
